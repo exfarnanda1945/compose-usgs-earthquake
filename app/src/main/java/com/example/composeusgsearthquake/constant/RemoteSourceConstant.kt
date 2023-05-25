61090969169d65f9665063b8a1cff450e0281a8f
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-05-25 09:47:09 +0700

    add remote source constant

---
+++ app/src/main/java/com/example/composeusgsearthquake/constant/RemoteSourceConstant.kt
@@ -0,0 +1,12 @@
+package com.example.composeusgsearthquake.constant
+
+object RemoteSourceConstant {
+    const val BASE_URL = "https://earthquake.usgs.gov/fdsnws/event/1/"
+
+    const val QUERY_END_TIME ="endtime"
+    const val QUERY_LIMIT ="limit"
+    const val QUERY_OFFSET ="offset"
+    const val QUERY_FORMAT ="format"
+    const val QUERY_ORDER_BY ="orderby"
+
+}
