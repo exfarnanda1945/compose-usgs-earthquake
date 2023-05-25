48297f3ff87ea05dbb9d888a813418838783a8cb
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-05-25 09:49:13 +0700

    add quake mapper dto to model

---
+++ app/src/main/java/com/example/composeusgsearthquake/data/mapper/QuakeMapper.kt
@@ -0,0 +1,24 @@
+package com.example.composeusgsearthquake.data.mapper
+
+import com.example.composeusgsearthquake.data.dto.QuakeDto
+import com.example.composeusgsearthquake.domain.model.Quake
+
+fun QuakeDto.toQuake():List<Quake>{
+    return this.features!!.map {
+        val item = it!!
+        val geometryItem = item.geometry!!
+        val propertiesItem = item.properties!!
+
+         Quake(
+            id = item.id ?: "",
+            type = propertiesItem.type ?: "",
+            tsunami = propertiesItem.tsunami == 1,
+            latitude = geometryItem.coordinates?.get(1) ?: 0.0,
+            longitude = geometryItem.coordinates?.get(2) ?: 0.0,
+            mag = propertiesItem.mag ?: 0.0,
+            place = propertiesItem.place ?: "",
+            time = propertiesItem.time ?: 0.toLong(),
+            title = propertiesItem.title ?: ""
+        )
+    }
+}
