ea8b15429a76218c00d6442a75aecb1068d7d285
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-04 21:28:20 +0700

    add remote data source constart

--- app/src/main/java/com/example/composeusgsearthquake/constant/RemoteSourceConstant.kt
+++ app/src/main/java/com/example/composeusgsearthquake/constant/RemoteSourceConstant.kt
@@ -2,11 +2,11 @@ package com.example.composeusgsearthquake.constant
 
 object RemoteSourceConstant {
     const val BASE_URL = "https://earthquake.usgs.gov/fdsnws/event/1/"
-
     const val QUERY_END_TIME ="endtime"
     const val QUERY_LIMIT ="limit"
     const val QUERY_OFFSET ="offset"
     const val QUERY_FORMAT ="format"
     const val QUERY_ORDER_BY ="orderby"
+    const val QUERY_MIN_MAG = "minmagnitude"
 
 }
