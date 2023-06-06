5826fc7b1a0524c87e10c6f01664826d7dd5fbf0
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-06 21:56:59 +0700

    add quake mapper from dto to Quake

--- app/src/main/java/com/example/composeusgsearthquake/data/mapper/QuakeMapper.kt
+++ app/src/main/java/com/example/composeusgsearthquake/data/mapper/QuakeMapper.kt
@@ -4,7 +4,7 @@ import com.example.composeusgsearthquake.data.dto.QuakeDto
 import com.example.composeusgsearthquake.domain.model.Quake
 
 fun QuakeDto.toQuake():List<Quake>{
-    return this.features!!.map {
+    return this.features!!.distinct().map {
         val item = it!!
         val geometryItem = item.geometry!!
         val propertiesItem = item.properties!!
@@ -14,7 +14,7 @@ fun QuakeDto.toQuake():List<Quake>{
             type = propertiesItem.type ?: "",
             tsunami = propertiesItem.tsunami == 1,
             latitude = geometryItem.coordinates?.get(1) ?: 0.0,
-            longitude = geometryItem.coordinates?.get(2) ?: 0.0,
+            longitude = geometryItem.coordinates?.get(0) ?: 0.0,
             mag = propertiesItem.mag ?: 0.0,
             place = propertiesItem.place ?: "",
             time = propertiesItem.time ?: 0.toLong(),
